e32b69b1f99f196ba7a45195733318f29389e21b
Debjani Mandal <66512818+Debjani966@users.noreply.github.com>
2024-11-22 11:46:49 +0530

    Delete QuizApplication directory

--- QuizApplication/src/main/java/com/example/QuizApplication/dao/QuizDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.QuizApplication.dao;
-
-import com.example.QuizApplication.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuizDao extends JpaRepository<Quiz,Integer> {
-}
